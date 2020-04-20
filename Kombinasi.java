b93b44a300d82899f65d02e8efd19c3f0c4b9021
Evinuria <63250961+Evinuria@users.noreply.github.com>
2020-04-20 21:57:59 +0700

    Add files via upload

---
+++ Kombinasi.java
@@ -0,0 +1,36 @@
+public class Kombinasi {
+    int []arr = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+    int []pilih ={1,2,3};
+    int i;
+    int j;
+    int n;
+    int r;
+    int s;
+    int fa,fb,fc; 
+    int kombinasi;
+    
+    public Kombinasi(){   
+}
+    public void kombinasi(){
+        fa=1; fb=1; fc=1; s=n-r; i=1; j=1;
+        if(s<=0){System.out.println("Angka yang anda masukkan salah");
+        }
+        else{
+        while(n>0){
+            fa=fa*n;
+            n=n-1;
+            }
+            while(r>0){
+            fb=fb*r;
+            r=r-1;
+            }
+            while (s>0){
+            System.out.println(s);
+            fc=fc*s;
+            s=s-1;
+            }
+        kombinasi= fa/(fb*fc);
+        System.out.println("Banyaknya cara \t:"+kombinasi);
+    }
+}
+}
---
+++ main.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.Scanner ;
+class main{
+    public static void main(String args[]){
+    int[] arr={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+        Scanner input = new Scanner (System.in);
+        System.out.println("KOMBINASI");
+        Kombinasi main;
+        main = new Kombinasi();
+        System.out.println("banyaknya bola yang berwarna dalam box =  ");
+        main.n=input.nextInt();
+        System.out.println("bola berwarna yang ingin diambil = ");
+        main.r = input.nextInt();
+        main.kombinasi();
+    }
+}
