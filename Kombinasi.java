5dde11d62d1007a6ffdba906d024c7c856a246d1
Evinuria <63250961+Evinuria@users.noreply.github.com>
2020-04-21 13:38:53 +0700

    Add files via upload

--- Kombinasi.java
+++ Kombinasi.java
@@ -1,6 +1,4 @@
 public class Kombinasi {
-    int[] arr = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-    int[] pilih ={1,2,3};
     int n;
     int r;
     int s;
--- main.java
+++ main.java
@@ -2,7 +2,6 @@ import java.util.Arrays;
 import java.util.Scanner ;
 class main{
     public static void main(String args[]){
-    int[] arr={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
         Scanner input = new Scanner (System.in);
         System.out.println("HELLO");
         System.out.println("MY NAME IS EVINURIA FILUYUNISTIARA");
