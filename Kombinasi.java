cdb85a88027f56763d1c4d6a877f6bed6368e521
Evinuria <63250961+Evinuria@users.noreply.github.com>
2020-04-21 07:50:53 +0700

    Add files via upload

--- Kombinasi.java
+++ Kombinasi.java
@@ -4,21 +4,23 @@ public class Kombinasi {
     int n;
     int r;
     int s;
-    int fa,fb,fc; 
+    int fa,fb,fc,fd; 
     int kombinasi;
     
     public Kombinasi(){   
 }
     public void kombinasi(){
-        fa=1; fb=1; fc=1; s=n-r;
+        fa=1; fb=1; fc=1; fd=1; s=n-r;
         if(s<=0){System.out.println("Angka yang anda masukkan salah");
         }
         else{
         while(n>0){
+            System.out.println(n);
             fa=fa*n;
             n=n-1;
             }
             while(r>0){
+            System.out.println(r);
             fb=fb*r;
             r=r-1;
             }
